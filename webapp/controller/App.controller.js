ad4ecbabfb6f58ba280258e68fb7437e6f3a1a0f
Shahabuddin Ansari <shahabuddin.ansari@tcs.com>
2025-09-07 13:02:22 +0000

    Initial Commit @9/7/2025, 1:02:22 PM

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("hds.ui5.todolistmultiselect.controller.App", {
+      onInit() {
+      }
+  });
+});
