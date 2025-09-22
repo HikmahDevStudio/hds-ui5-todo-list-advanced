b7a2e2ba4296261dcff36dc15428d752565e63c3
Shahabuddin Ansari <shahabuddinansari@Shahabuddin.local>
2025-09-22 21:26:59 +0530

    Update liveChange issues

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,9 +10,52 @@ sap.ui.define(
         this.addTaskDialogBoxHandler();
       },
 
-      inputTaskOnChangeHandler(oEvent) {
-        const sInput = oEvent.getParameter("value");
-        console.log(sInput);
+      inputTaskLiveChange(oEvent) {
+        const oInputDesc = this.getView().byId("idInputTaskDescription");
+        const oTaskDueDate = this.getView().byId("idDatePickerInputTask");
+        const oTaskPriority = this.getView().byId("idPriorityInputTask");
+        const oTaskCategory = this.getView().byId("idCategoryInputTask");
+
+        const selectedControldId = oEvent.getParameter("id");
+        const selectedDropDownControl = oEvent
+          .getParameter("selectedItem")
+          ?.getId();
+
+        // Input Task Description handling
+        if (selectedControldId?.includes("idInputTaskDescription")) {
+          const isValidInput = oEvent.getParameter("value")?.trim()?.length > 0;
+          const isErrorState = oInputDesc.getValueState() === "Error";
+
+          // Remove previously Error state set on input field if valid input provided
+          if (isErrorState && isValidInput) oInputDesc.setValueState("None");
+        }
+
+        // Due Date input handling
+        if (selectedControldId?.includes("idDatePickerInputTask")) {
+          const isValidInput = oEvent.getParameter("value")?.length > 0;
+          const isErrorState = oTaskDueDate.getValueState() === "Error";
+
+          // Remove previously Error state set on input field if valid input provided
+          if (isErrorState && isValidInput) oTaskDueDate.setValueState("None");
+        }
+
+        // Priority input handling
+        if (selectedDropDownControl?.includes("idPriorityInputTask")) {
+          const isValidInput = oEvent.getParameter("selectedItem").getKey();
+          const isErrorState = oTaskPriority.getValueState() === "Error";
+
+          // Remove previously Error state set on input field if valid input provided
+          if (isErrorState && isValidInput) oTaskPriority.setValueState("None");
+        }
+
+        // Category input handling
+        if (selectedDropDownControl?.includes("idCategoryInputTask")) {
+          const isValidInput = oEvent.getParameter("selectedItem").getKey();
+          const isErrorState = oTaskCategory.getValueState() === "Error";
+
+          // Remove previously Error state set on input field if valid input provided
+          if (isErrorState && isValidInput) oTaskCategory.setValueState("None");
+        }
       },
 
       openAddTaskDialog() {
@@ -31,13 +74,16 @@ sap.ui.define(
         const taskDescription = this.getView()
           .byId("idInputTaskDescription")
           .getValue();
+
         const taskDueDate = this.getView()
           .byId("idDatePickerInputTask")
           .getValue();
+
         const taskPriority = this.getView()
           .byId("idPriorityInputTask")
           .getSelectedItem()
           .getText();
+
         const taskCategory = this.getView()
           .byId("idCategoryInputTask")
           .getSelectedItem()
@@ -64,6 +110,8 @@ sap.ui.define(
         const oModel = this.getView().getModel();
         const aTaskList = oModel.getProperty("/tasks") || [];
         oModel.setProperty("/tasks", [...aTaskList, oInputTask]);
+
+        this.resetInputTasksFields();
         this.byId("idDialogMain").close();
       },
 
@@ -105,16 +153,26 @@ sap.ui.define(
 
       resetInputTasksFields() {
         const oInputDesc = this.getView().byId("idInputTaskDescription");
+        const oTaskDueDate = this.getView().byId("idDatePickerInputTask");
+        const oTaskPriority = this.getView().byId("idPriorityInputTask");
+        const oTaskCategory = this.getView().byId("idCategoryInputTask");
+
+        oInputDesc.setValue("");
         oInputDesc.setValueState("None");
         oInputDesc.setValueStateText(null);
 
-        this.getView().byId("idInputTaskDescription").setValue("");
-        this.getView().byId("idDatePickerInputTask").setValue(null);
-        this.getView().byId("idPriorityInputTask").setSelectedItem(null);
-        this.getView().byId("idCategoryInputTask").setSelectedItem(null);
+        oTaskDueDate.setValue(null);
+        oTaskDueDate.setValueState("None");
+
+        oTaskPriority.setSelectedKey("");
+        oTaskPriority.setValueState("None");
+
+        oTaskCategory.setSelectedKey("");
+        oTaskCategory.setValueState("None");
       },
 
       cancelTaskDialogBoxHandler() {
+        this.resetInputTasksFields();
         this.byId("idDialogMain").close();
       },
 
