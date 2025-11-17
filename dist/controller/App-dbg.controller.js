5aeec606de8e4705aee3b99e4a4668b694329f40
Shahabuddin Ansari <shahabuddinansari@Shahabuddin.local>
2025-11-17 21:26:54 +0530

    build created

---
+++ dist/controller/App-dbg.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("hds.ui5.todolistadvance.controller.App", {
+    onInit() {},
+  });
+});
---
+++ dist/test/integration/AllJourneys.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/test/Opa5","./arrangements/Startup","./NavigationJourney"],function(e,t){"use strict";e.extendConfig({arrangements:new t,viewNamespace:"hds.ui5.todolistmultiselect.view.",autoWait:true})});
+//# sourceMappingURL=AllJourneys.js.map
